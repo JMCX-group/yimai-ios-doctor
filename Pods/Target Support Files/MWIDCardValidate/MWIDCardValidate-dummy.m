53d66fab6ba405b2224b649929c55ace00dc101a
why <why>
2016-07-12 16:06:18 +0800

    remove some dir and files

--- Pods/Target Support Files/MWIDCardValidate/MWIDCardValidate-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MWIDCardValidate : NSObject
-@end
-@implementation PodsDummy_MWIDCardValidate
-@end
--- Pods/Target Support Files/MWIDCardValidate/MWIDCardValidate-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "MWIDCardValidate.h"
-
-FOUNDATION_EXPORT double MWIDCardValidateVersionNumber;
-FOUNDATION_EXPORT const unsigned char MWIDCardValidateVersionString[];
-
